803803ed70de3400723619602aed82600ea37d84
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2024-10-03 16:14:21 +0700

    remove debug prod

--- src/app/api/spotify/top-tracks/route.ts
+++ src/app/api/spotify/top-tracks/route.ts
@@ -6,9 +6,7 @@ export const GET = async () => {
   const response = await getTopTracks();
 
   if (response.status > 200) {
-    return new Response(JSON.stringify({ success: false, response }), {
-      status: 200,
-    });
+    return new Response(JSON.stringify({ success: false }), { status: 200 });
   }
 
   return new Response(
